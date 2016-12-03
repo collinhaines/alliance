7e4683b4bae52263b0c1b712d31137ddc7872805
Collin Haines <collinhaines@me.com>
2016-12-03 18:18:15 -0500

    Adjust Navigation

--- imports/ui/components/navigation.js
+++ imports/ui/components/navigation.js
@@ -1,27 +1,4 @@
-/*!
- * alliance
- *
- * Copyright 2016 Collin Haines
- * Licensed under the MIT license.
- */
+import './navigation.html';
 
-Template.navigation.events({
-  'click #navigation a': function (event) {
-    if ($('#navigation li.active').length) {
-      $('#navigation li.active').removeClass('active').find('span').remove();
-    } // if ($('#navigation li.active').length)
-
-    $(event.target).append(' <span class="sr-only">(current)</span>').parent().addClass('active');
-
-    if ($('.navbar-collapse').hasClass('in')) {
-      $('.navbar-toggle').trigger('click');
-    } // if ($('.navbar-collapse').hasClass('in'))
-
-    $('html, body').animate({
-      scrollTop: $(event.target.hash).position().top
-    });
-
-    event.target.blur();
-    event.preventDefault();
-  } // 'click #navigation a': function (event)
-});
+import '/imports/library/collapse.js';
+import '/imports/library/transition.js';
