d352eb2dc44ec9e4d20d0da54db4e409729ba177
Collin Haines <collinhaines@me.com>
2016-12-11 19:34:20 -0500

    Added Modernizr Alerts
    Closes #1

--- imports/ui/layout/overlord.js
+++ imports/ui/layout/overlord.js
@@ -1,3 +1,14 @@
 import './overlord.html';
 
+import { Template } from 'meteor/templating';
+import { $ } from 'meteor/jquery';
+
 import '/imports/library/modernizr.js';
+
+Template.overlord.onRendered(function () {
+  if (Modernizr.cssanimations) {
+    console.info('Hiding ' + $('.cssanimations-alert').length + ' cssanimations alerts.');
+
+    $('.cssanimations-alert').hide();
+  }
+});
