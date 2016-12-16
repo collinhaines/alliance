1f41bad605947926afebab2f2e9e248520e88a58
Collin Haines <collinhaines@me.com>
2016-12-15 20:33:02 -0500

    Close Navigation on Smaller Screens

--- imports/ui/components/navigation.js
+++ imports/ui/components/navigation.js
@@ -1,4 +1,13 @@
 import './navigation.html';
 
+import { Template } from 'meteor/templating';
+import { $ } from 'meteor/jquery';
+
 import '/imports/library/collapse.js';
 import '/imports/library/transition.js';
+
+Template.navigation.events({
+  'click .navbar-nav a'(event) {
+    $('#navigation').collapse('hide');
+  }
+});
